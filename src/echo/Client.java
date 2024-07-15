478ec9b6c6d672349734c971e947801b0083d9fe
antrophus <antrophus@yahoo.com>
2024-07-15 17:30:55 +0900

    7/15 TCP 소켓 통신

---
+++ src/echo/Client.java
@@ -0,0 +1,22 @@
+package echo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException{
+		
+		Socket socket = new Socket();
+		System.out.println("<클라이언트 시작>");
+		System.out.println("========================================");
+		
+		System.out.println("[서버에 연결을 요청합니다.]");
+		socket.connect(new InetSocketAddress("192.168.0.62", 10001));
+		
+		System.out.println("[서버에 연결 되었습니다.]");
+
+	}
+
+}
---
+++ src/echo/server.java
@@ -0,0 +1,26 @@
+package echo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+	
+	public static void main(String[] args) throws IOException {
+		//소켓생성
+		ServerSocket serverSocket = new ServerSocket();
+		//바인드 ip=192.168.0.62  port=10001 여기에 프로그램 구동중
+		serverSocket.bind(new InetSocketAddress("192.168.0.62", 10001));
+		
+		//서버시작
+		System.out.println("<서버시작>");
+		System.out.println("============================================");
+		System.out.println("연결을 기다리고 있습니다.");
+		
+		//socket은 종이컵 전화기 : 대기상태 ->클라이언트가 오면 serverSocket.에서 new socket을 빼서 만들어준다.
+		Socket socket = serverSocket.accept();
+		System.out.println("클라이언트가 연결 되었습니다.");
+	}
+
+}
