a2413dfc47659f3f60722cda529654b4788c273e
antrophus <antrophus@yahoo.com>
2024-07-15 18:05:06 +0900

    7/15 소켓 통신 연결해보기

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -13,7 +13,7 @@ public class Client {
 		System.out.println("========================================");
 		
 		System.out.println("[서버에 연결을 요청합니다.]");
-		socket.connect(new InetSocketAddress("192.168.0.62", 10001));
+		socket.connect(new InetSocketAddress("192.168.0.62", 10001)); //학원 컴퓨터 IP임. 
 		
 		System.out.println("[서버에 연결 되었습니다.]");
 
